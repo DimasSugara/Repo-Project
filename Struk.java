3815c8979c7e9408144dced7139f9505bc62299d
Kelompok Gatau <dimassugarareal@gmail.com>
2020-12-23 16:25:48 +0700

    Commit Kedua Fitur Struk dan Sub-Fitur Koki

---
+++ Struk.java
@@ -0,0 +1,8 @@
+public interface Struk{
+    String[]strukNama = new String[5];
+    String[]strukMeja = new String[1];
+    int[]strukHarga = new int[5];
+    int[]strukJumlah = new int[5];
+    
+    public void cetakStruk();
+}
