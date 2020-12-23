77cb7d710682d46087a8492416416575f65c5c4c
Sri Ulina Raftaini <sriulinaraftaini20@gmail.com>
2020-12-23 15:44:06 +0700

    Commit pertama Fitur Pelayan

---
+++ Pelayan.java
@@ -0,0 +1,19 @@
+public class Pelayan {
+    public void setPelayan (String namaPel, String id) {
+    }
+
+	public String getNamaPel() {
+    }
+	
+	public void terimaPesanan(Pelanggan p1, Kasir kasir1) {
+    }
+
+	public void delay() {
+    }
+
+	public void delayMasak() {
+    }
+	
+	public void laporMenu() {
+    }
+}
