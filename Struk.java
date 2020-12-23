97e6d92cea6d2fd611a03358fdd59dce20033a80
DimasSugara <66243353+DimasSugara@users.noreply.github.com>
2020-12-23 15:59:18 +0700

    Add files via upload
    Acc Perbaikan

--- Struk.java
+++ Struk.java
@@ -1,7 +1,7 @@
 public interface Struk{
     String[]strukNama = new String[5];
-	String[]strukMeja = new String[1];
-	int[]strukHarga = new int[5];
+    String[]strukMeja = new String[1];
+    int[]strukHarga = new int[5];
     int[]strukJumlah = new int[5];
     
     public void cetakStruk();
