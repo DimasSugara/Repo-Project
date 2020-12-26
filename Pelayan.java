7b2b2a35d0942967a6b7c25c84d716f0bebef553
Sri Ulina Raftaini <sriulinaraftaini20@gmail.com>
2020-12-26 22:53:00 +0700

    Commit kesekian kalinya

--- Pelayan.java
+++ Pelayan.java
@@ -1,19 +1,43 @@
-public class Pelayan {
-    public void setPelayan (String namaPel, String id) {
-    }
-
-	public String getNamaPel() {
-    }
+public class Pelayan  {
 	
-	public void terimaPesanan(Pelanggan p1, Kasir kasir1) {
-    }
 
-	public void delay() {
-    }
-
-	public void delayMasak() {
-    }
+String [] namaPel= {
+			"Budi","Samsol","Jamal","Doubleh","Asep", "Bambang"
+			};
+	public void daftarPelayan() {
+		System.out.println("Daftar Pelayan : ");
+		for (int i=0 ; i<5 ; i++) {
+		System.out.println((i+1)+". " + namaPel[i]);
+	}
+}
 	
-	public void laporMenu() {
-    }
+public void terimaPesanan(Pelanggan p1, Kasir kasir1) {
+	System.out.println("Pesanan telah diterima oleh Pelayan");
+	p1.pesanan(kasir1);
+}
+
+public void delay() {
+	try {
+		Thread.sleep(1000);
+	}
+	catch(InterruptedException ex)
+	{
+		Thread.currentThread().interrupt();
+	}
+}
+
+public void delayMasak() {
+	try {
+		Thread.sleep(5000);
+	}
+	catch(InterruptedException ex)
+	{
+		Thread.currentThread().interrupt();
+	}
+}
+	
+public void laporMenu() {
+	System.out.println("Pelayan memberikan pesanan pelanggan ke Koki\n\n");
+}
+	
 }
