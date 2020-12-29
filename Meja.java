9b0b718594e0228277c1b0a0d5500d8fae4cd686
Sri Ulina Raftaini <sriulinaraftaini20@gmail.com>
2020-12-30 00:39:27 +0700

    Push code meja

--- Meja.java
+++ Meja.java
@@ -1,11 +1,11 @@
 public class Meja {
 	
 	String [] namaMeja = {
-			"Meja 01","Meja 02","Meja 03","Meja 04","Meja 05"
-			};
+		"Meja 01","Meja 02","Meja 03","Meja 04","Meja 05"
+	};
 
-			public void daftarMeja() {
-			System.out.println("Daftar Meja : ");
+	public void daftarMeja() {
+		System.out.println("Daftar Meja : ");
 		for (int i=0 ; i<5 ; i++) {
 			System.out.println((i+1)+". " + namaMeja[i]);
 		}
