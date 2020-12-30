c74c26f80fb02c0eff770026d6f22b29626f3668
DimasSugara <66243353+DimasSugara@users.noreply.github.com>
2020-12-30 08:10:15 +0700

    Update Meja.java
    MEJA PULL ERROR

--- Meja.java
+++ Meja.java
@@ -4,6 +4,7 @@ public class Meja {
 		"Meja 01","Meja 02","Meja 03","Meja 04","Meja 05"
 	};
 
+	
 	public void daftarMeja() {
 		System.out.println("Daftar Meja : ");
 		for (int i=0 ; i<5 ; i++) {
